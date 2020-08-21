3d42d6f0157f31410addbaeafca39a65c4c6b2fa
nitesh <nitesh@agsft.com>
2020-08-22 04:07:11 +0530

    v 1.9.0

--- main.js
+++ main.js
@@ -18,9 +18,9 @@ function createWindow() {
   mainWindow.on("closed", function () {
     mainWindow = null;
   });
-  mainWindow.once("ready-to-show", () => {
+ 
     autoUpdater.checkForUpdatesAndNotify();
-  });
+
 }
 
 app.whenReady().then(() => {
